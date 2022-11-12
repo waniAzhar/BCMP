ec9443458a07cea1bc3fdcc646151c5b08f6f135
waniAzhar <waniazharfarooq@gmail.com>
2022-11-12 23:59:30 +0530

    first commit

---
+++ src/app/_model/product.model.ts
@@ -0,0 +1,7 @@
+export interface Product {
+    productName: string,
+    productDescription: string,
+    productCategory: string,
+    productDiscountedPrice: number,
+    productActualPrice: number
+}
